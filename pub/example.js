adb90553997a247e1460fa3d69ecbc9bc09dca48
ekluthra <ekluthra@yahoo.com>
2021-11-23 21:32:09 -0500

    moved images to pub

--- pub/example.js
+++ pub/example.js
@@ -3,7 +3,7 @@
 const rp = new RecipePage()
 rp.makeCard(300, 340, '#FFF7DD', 'Pesto Pasta', ['Chicken Breast', 'Spaghetti', 'Tomato Sauce', 'Mozarella Cheese', 'Cilantro'])
 rp.setTitle('Chicken Parmasean') //This allows the developer to be able to change the title if they want to.
-rp.setImage('../images/chicken-parmesan.jpg') // as well as changing the image
+rp.setImage('../pub/images/chicken-parmesan.jpg') // as well as changing the image 
 //I wanted a separate function for setting the description because putting the description in the makeCard function as an argument would
 //result in a very large line that doesn't look easy to read for developers
 
@@ -19,4 +19,4 @@ rp2.makeCard(400, 560, '#5F9EA0', 'Butter Chicken', ['Chicken Thigh', 'Plain Yog
 Heat butter or ghee in the same pan. Fry the onions until they start to sweat (about 6 minutes) while scraping up any browned bits stuck on the bottom of the pan. \
 Add crushed tomatoes, chili powder and salt. Let simmer for about 10-15 minutes, stirring occasionally until sauce thickens and becomes a deep brown red colour. \
 Remove from heat, scoop mixture into a blender and blend until smooth. You may need to add a couple tablespoons of water to help it blend (up to 1/4 cup). \
-Work in batches depending on the size of your blender.', '../images/butter-chicken.jpg')
+Work in batches depending on the size of your blender.', '../pub/images/butter-chicken.jpg')
