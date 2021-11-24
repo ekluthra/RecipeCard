c77b0a1e3803ec5f4d39068326fce4fae54c291e
ekluthra <ekluthra@yahoo.com>
2021-11-23 19:59:29 -0500

    removed unnecessary functions

--- server.js
+++ server.js
@@ -5,17 +5,6 @@ const app = express();
 const path = require('path')
 
 app.use(express.static(path.join(__dirname, '/pub')))
-app.get('/', () => {
-})
-
-// Error codes
-app.get('/problem', (res) => {
-	res.status(500).send('There was a problem on the server')
-})
-
-// Sending some JSON
-app.get('/someJSON', () => {
-})
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
