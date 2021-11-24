369a4e3bf4b9cf56b687cf6e4a0ea3791bef75aa
ekluthra <ekluthra@yahoo.com>
2021-11-23 21:28:53 -0500

    added package

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname, '/pub')))
 
+app.get('/', (req, res) => {
+    // sending a string
+    //res.send('This should be the root route!')
+
+    //sending some HTML
+})
+
 const port = process.env.PORT || 5000
 app.listen(port, () => {
 	console.log(`Listening on port ${port}...`)
