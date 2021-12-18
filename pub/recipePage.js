106c041a0079e3f634d99f41428307cbfb65b6e4
ekluthra <ekluthra@yahoo.com>
2021-12-18 14:13:11 -0500

    Feature: recipe tags, theme setting, unflippable cards

--- pub/recipePage.js
+++ pub/recipePage.js
@@ -24,11 +24,15 @@ function RecipePage() {
 
 RecipePage.prototype = {
     
-    makeCard: function(width, height, color, title, ingredients, description, image) {
+    makeCard: function(width, height, color, title, ingredients, description, image, tags, parent, flippable=true) {
+        this.flippable = flippable
+
         const card  = document.createElement('div')
         card.classList.add('card')
-        card.classList.add(`cardId-${this.id}`)
-        card.setAttribute('onClick', `flipCard(${this.id})`)
+        card.classList.add(`cardId-${this.id}`);
+        if(this.flippable){
+            card.setAttribute('onClick', `flipCard(${this.id})`)
+        }
 
         const cardContainer = document.createElement('div')
         cardContainer.style = `width: ${width}px; height: ${height}px; margin: 10px; background-color:${color}; border-radius:20px; box-shadow: 5px 10px 8px #888888; outline:1px solid black;`
@@ -41,36 +45,59 @@ RecipePage.prototype = {
         const cardBack = document.createElement('div')
         cardBack.className = 'card-back'
         cardBack.style  = 'position: absolute; height:100%; width: 100%;'
+        
+        const unflippableFront = document.createElement('div')
+        unflippableFront.className = 'unflippable-front'
+        unflippableFront.style = 'position: absolute; height:100%; width:100%;'
 
         this.cardTitleDiv.style = 'height:10%; text-align:center;'
         this.setTitle(title);
-        cardFront.append(this.cardTitleDiv)
+        (this.flippable ? cardFront.append(this.cardTitleDiv) : unflippableFront.append(this.cardTitleDiv))
 
         this.imageDiv.style = 'height:40%; padding:5px;'
-        this.setImage(image)
-        cardFront.append(this.imageDiv)
+        this.setImage(image);
+        (this.flippable ? cardFront.append(this.imageDiv) : unflippableFront.append(this.imageDiv))
 
         this.ingredientsDiv.style='max-height:40%; padding:2px;'
-        this.addIngredients(ingredients)
-        this.ingredientsDiv.setAttribute('onClick', 'event.stopPropagation();')
-        cardFront.append(this.ingredientsDiv)
+        this.addIngredients(ingredients);
+        this.ingredientsDiv.setAttribute('onClick', 'event.stopPropagation();');
+        (this.flippable ? cardFront.append(this.ingredientsDiv) : unflippableFront.append(this.ingredientsDiv))
+
+        this.tagsDiv.className = 'tags'
+        this.addTags(tags);
+        (this.flippable ? cardFront.append(this.tagsDiv) : unflippableFront.append(this.tagsDiv))
 
         this.descriptionDiv.style='margin:10px; padding:10px;'
-        this.setDescription(description)
-        cardBack.append(this.descriptionDiv)
-
-        card.append(cardFront)
-        card.append(cardBack)
+        this.setDescription(description);
+        (this.flippable ? cardBack.append(this.descriptionDiv) : unflippableFront.append(this.descriptionDiv))
+        if(this.flippable){
+            card.append(cardFront)
+            card.append(cardBack)
+        }else{
+            card.append(unflippableFront)
+        }
+        
         cardContainer.append(card)
 
         recipePages.push(card)
 
-        const body = document.querySelector('body')
+        const body = document.getElementById(parent)
         body.append(cardContainer)
         numRecipes++;
-        console.log(this.id)
-        console.log(numRecipes)
-        console.log(recipePages)
+    },
+
+    addTags: function(tags){
+        tags.forEach(function(tag){
+            this.tags.push(tag)
+            const tagDiv = document.createElement('div')
+            if(tag==='spicy'){
+                tagDiv.innerHTML = '<img src = "./images/spicy.png" style="width:35px; float:left; margin:5px;"/>'
+            }
+            if(tag==='vegetarian'){
+                tagDiv.innerHTML = '<img src = "./images/vegetarian.png" style="width:35px; float:left; margin:5px;"/>'
+            }
+            this.tagsDiv.append(tagDiv)
+        }.bind(this));
     },
 
     setTitle: function(title){
@@ -78,6 +105,7 @@ RecipePage.prototype = {
         this.cardTitleDiv.innerHTML = `<h3> ${title} </h3>`
     },
 
+    
     addIngredients: function(ingredients){
         this.ingredients.push(...ingredients)
         const ingredientsList = document.createElement("ul")
@@ -103,4 +131,8 @@ RecipePage.prototype = {
 // flipping functionality reference used: https://jsfiddle.net/james2doyle/qsQun/
 function flipCard(cardId) {
     $(`.cardId-${cardId}`).toggleClass('flipped');
+}
+
+function setTheme(theme) {
+    $('.card-container').css('background-color', theme);
 }
